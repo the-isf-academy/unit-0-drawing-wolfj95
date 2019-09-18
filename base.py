594f35a05028d8092ae4a47967b10525aded3527
Jacob Wolf <jacob.h.wolf@gmail.com>
2019-09-18 16:29:55 +0800

    Added comments
    Commented each of the functions in project.py and base.py to include a description of the function, a description of the parameters the function takes, and a description of the value the function returns.

--- base.py
+++ base.py
@@ -4,6 +4,15 @@ from math import sqrt, pow
 from helpers import restore_state_when_finished
 
 def draw_oval(a, b):
+    """
+    Draws an oval centered around the Turtle's starting position by defining
+    points around the oval and drawing lines between them.
+
+    a is a numebr representing half of the oval's width
+    b is a number representing half of the oval's height
+
+    returns the points used to draw the oval.
+    """
     points = []
     with restore_state_when_finished():
         enter_position = position()
@@ -21,27 +30,49 @@ def draw_oval(a, b):
         points.append(position())
     return(points)
 
-def draw_cylinder(radius, height, depth, color):
+def draw_cylinder(radius, height, x_rotation, color):
+    """
+    Draws a cylinder centered around the Turtle's starting position by drawing
+    an over and then adding perspective to that oval with an origin defined
+    below the oval. The oval is then projected toward that origin to create a
+    3D cylinder.
+
+    radius is a number representing the radius of the top circle of the cylinder
+    height is a number representing the height of the water bottle
+    x_rotation is a number between 0 and 1 used to determine the ratio of width
+        to height of the oval (creating a perception of depth)
+    color is a color used to  color of the water bottle
+
+    returns the points used to draw the top oval of the cylinder.
+    """
     with restore_state_when_finished():
         penup()
         setheading(270)
         forward(height/2)
         pendown()
         pencolor('gray')
-        draw_oval(radius, radius*depth)
+        draw_oval(radius, radius*x_rotation)
         penup()
         setheading(90)
         forward(height)
         pendown()
         pencolor(color)
-        oval_points = draw_oval(radius, radius*depth)
+        oval_points = draw_oval(radius, radius*x_rotation)
         add_perspective(oval_points, [0, 100*height], -.01)
         pencolor('gray')
-        draw_oval(radius, (radius)*depth)
+        draw_oval(radius, (radius)*x_rotation)
         penup()
     return oval_points
 
 def draw_taper(larger_oval, smaller_oval):
+    """
+    Draws a taper between a smaller oval and a larger oval assigning each point
+    on the larger oval to a point on the smaller circle and drawing lines
+    between them.
+
+    larger_oval is a set of points represnting the points around the larger oval
+    smaller_oval is a set of points represnting the points around the smaller oval
+    """
     with restore_state_when_finished():
         prev_smaller_oval_index = 0
         prev_smaller_oval_point = smaller_oval[prev_smaller_oval_index]
@@ -62,15 +93,31 @@ def draw_taper(larger_oval, smaller_oval):
 
 
 def draw_base(dimensions):
-    top_cylinder_points = draw_cylinder(dimensions['bottle_radius'], dimensions['bottle_height'], dimensions['depth'], dimensions['color'])
+    """
+    Draws the base of the bottle by drawing a cylinder, drawing an oval for the
+    lip, and then drawing a taper between the top oval of the cylinder and the
+    lip oval.
+
+    dimensions is a dictionary of settings used to draw the bottle, defined in
+        project.py
+    """
+
+    top_cylinder_points = draw_cylinder(dimensions['bottle_radius'], dimensions['bottle_height'], dimensions['x_rotation'], dimensions['color'])
     pencolor('gray')
     fly(0, dimensions['bottle_height']/2+dimensions['taper_height'])
     pensize(dimensions['lip_radius']-dimensions['cap_radius'])
-    lip_points = draw_oval(dimensions['lip_radius'], (dimensions['lip_radius'])*dimensions['depth'])
+    lip_points = draw_oval(dimensions['lip_radius'], (dimensions['lip_radius'])*dimensions['x_rotation'])
     pensize(1)
     pencolor(dimensions['color'])
     draw_taper(top_cylinder_points, lip_points)
 
 def draw_cap(dimensions):
+    """
+    Draws the cap of the bottle by drawing a cylinder that begins above the
+    lip oval.
+
+    dimensions is a dictionary of settings used to draw the bottle, defined in
+        project.py
+    """
     fly(0, dimensions['bottle_height']/2+dimensions['cap_height']/2+dimensions['taper_height'])
-    draw_cylinder(dimensions['cap_radius'], dimensions['cap_height'], dimensions['depth'], 'black')
+    draw_cylinder(dimensions['cap_radius'], dimensions['cap_height'], dimensions['x_rotation'], 'black')
--- project.py
+++ project.py
@@ -6,20 +6,30 @@ from base import draw_base, draw_cap
 from turtle import hideturtle
 
 def default_bottle_dimensions():
+    """
+    Returns a dictionary containing key-value pairs for the drawing settings for my water bottle.
+    height and radius values are in standard Turtle units
+    angle values are in degrees
+    rotation values are values between 0 and 1 representing the rotation of the bottle around an axis
+    color is a color value used by the Turtle pen
+    """
     return {
-        'bottle_radius': 75,
+        'bottle_radius': 75.5,
         'bottle_height': 325,
         'lip_radius': 53,
         'taper_height': 15,
         'cap_radius': 50,
         'cap_height': 40,
         'handle_angle': 45,
-        'bottle_rotation': 0,
-        'depth': 1,
+        'y_rotation': 0,
+        'x_rotation': .2,
         'color': 'yellow'
     }
 
 if __name__ == '__main__':
+    """
+    Draws the bottle by calling functions from the base module using the no_delay context manage from the helpers module
+    """
     dimensions = default_bottle_dimensions()
     with no_delay():
         draw_base(dimensions)
